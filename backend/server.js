48e622ccb66b840fc3a1e77980e7da07106b1e67
FaizAhemad <sfaiz57@gmail.com>
2025-07-20 06:13:45 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 // ✅ Define allowed frontend origins
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://learn-deployment-cd51yhfy4-faiz-ahemads-projects.vercel.app/' // Replace with your actual deployed frontend URL
+    'https://learn-deployment-cd51yhfy4-faiz-ahemads-projects.vercel.app' // Replace with your actual deployed frontend URL
 ];
 
 
